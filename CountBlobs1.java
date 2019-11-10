a07f16dd256b4852b4b7ae7b809a555529f43079
irisqaz <jgarc172@mail.ccsf.edu>
2019-11-09 20:01:30 -0800

    boundary checks at top

--- CountBlobs1.java
+++ CountBlobs1.java
@@ -19,7 +19,8 @@ public class CountBlobs1 {
                 if (matrix[i] == 1 && visited[i] == 0) // unvisited black cell
                 {
                     count++;
-                    markVisited(i, matrix, visited, rowCount);
+                    int visits = markVisited(i, matrix, visited, rowCount);
+                    System.out.printf("for %d, visited : %d \n", i, visits);
                 }
         }
 
@@ -27,22 +28,27 @@ public class CountBlobs1 {
     }
 
     static int markVisited(int i, int[] matrix, int[] visited, int rowCount) {
-        if (i < 0)
+        if (!isIndex(i,rowCount) || matrix[i] == 0 || visited[i] == 1){
             return 0;
+        }
 
-        if (i >= rowCount)
-            return 0;
+        //if (i >= rowCount)
+        //    return 0;
 
-        if (visited[i] == 1) // already visited
-            return 1;
+        //if (visited[i] == 1) // already visited
+        //    return 1;
 
-        if (matrix[i] == 0) // not a black cell
-            return 0;
+        //if (matrix[i] == 0) // not a black cell
+        //    return 0;
 
         visited[i] = 1;
-
+        int count = 1;
         // recursively mark all the 4 adjacent cells - right, left, up and down
-        return markVisited(i + 1, matrix, visited, rowCount)
-                + markVisited(i - 1, matrix, visited, rowCount);
+        return count + markVisited(i + 1, matrix, visited, rowCount);
+                //+ markVisited(i - 1, matrix, visited, rowCount);
+    }
+
+    private static boolean isIndex(int i, int rowCount) {
+        return i >= 0 && i < rowCount ;
     }
 }
