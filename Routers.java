4392167ef96fc26ae8c2a155ca396918f4b37814
irisqaz <jgarc172@mail.ccsf.edu>
2019-11-12 20:26:37 -0800

    created adjList

--- Routers.java
+++ Routers.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -32,6 +33,9 @@ public class Routers {
     static List<Integer> criticalRouters(int numRouters, int numLinks, List<List<Integer>> links) {
         List<Integer> critical = new ArrayList<Integer>();
 
+        List<List<Integer>> adjList = adjacentList(links, numRouters);
+        printAdjList(adjList);
+        
         for (int i = 0; i < numRouters; i++) {
             if (isCritical(i)){
                 critical.add(i);
@@ -40,9 +44,34 @@ public class Routers {
 
         return critical;
     }
+
+    private static ArrayList<List<Integer>> adjacentList(List<List<Integer>> links, int numRouters) {
+        ArrayList<List<Integer>> adj = new ArrayList<>(numRouters);
+        // initialize
+        for (int i = 0; i < numRouters; i++) {
+            adj.add(new LinkedList<Integer>());
+        }
+        for (int i = 0; i < numRouters; i++) {
+            List<Integer> edge = links.get(i);
+            Integer v1 = edge.get(0);
+            Integer v2 = edge.get(1);
+            adj.get(v1).add(v2);
+            adj.get(v2).add(v1);
+        }
+        return adj;
+    }
     
     private static boolean isCritical(int i) {
         return false;
     }
+    
+    private static void printAdjList(List<List<Integer>> adjList) {
+        System.out.println("\n");
+        for (int i = 0; i < adjList.size(); i++) {
+            System.out.printf("[%d] -> %s \n", i, adjList.get(i));
+        }
+        System.out.println();
+    }
+
 
 }
