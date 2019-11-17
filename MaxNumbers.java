c15cd7a509a9cd452646449481c3fa4203950dde
irisqaz <jgarc172@mail.ccsf.edu>
2019-11-17 15:40:11 -0800

    maxnumbers ready for quiz

--- MaxNumbers.java
+++ MaxNumbers.java
@@ -6,7 +6,55 @@ import java.util.Random;
  */
 public class MaxNumbers {
 
+    // find2Max returns the two largest values
+    // in nums.  nums in at least length 2
     private static int[] find2Max(int[] nums) {
+        int[] res = new int[2];
+        
+        return res;
+    }
+    public static void main(String[] args) {
+
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+
+            int N = getInt(2, 10);
+            int[] nums = getNums(N, 100);
+            String padded = String.format("%1$40s", Arrays.toString(nums));
+            System.out.print(padded);
+
+            int[] result = find2Max(nums);
+            int[] soln = solution(nums);
+
+            System.out.printf(" -> %s ", Arrays.toString(result));
+            if (!Arrays.equals(result, soln)){
+                System.out.printf(" X expected: %s ", Arrays.toString(soln));
+            } else {
+                System.out.print(" correct");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static int[] getNums(int length, int size) {
+        // return an array of integers with values
+        // between length and size
+        int[] nums = new int[length];
+
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            nums[i] = rand.nextInt(size);
+        }
+
+        return nums;
+    }
+
+    private static int getInt(int min, int max) {
+        int spread = max - min;
+        return new Random().nextInt(spread + 1) + min;
+    }
+    private static int[] solution(int[] nums) {
         int[] result = new int[2];
         int max1 = 0;
         int max2 = 0;
@@ -28,36 +76,5 @@ public class MaxNumbers {
         result[1] = max2;
         return result;
     }
-    public static void main(String[] args) {
-
-        System.out.println();
-        for (int i = 0; i < 5; i++) {
-
-            int N = getRandomIntegerWithinRange(2, 10);
-            int[] nums = getNums(N, 100);
-            System.out.print(Arrays.toString(nums));
-            int[] result = find2Max(nums);
-            System.out.printf(" -> %s \n", Arrays.toString(result));
-        }
-        System.out.println();
-    }
-
-    private static int[] getNums(int length, int size) {
-        // return an array of integers with values
-        // between length and size
-        int[] nums = new int[length];
-
-        Random rand = new Random();
-        for (int i = 0; i < length; i++) {
-            nums[i] = rand.nextInt(size);
-        }
-
-        return nums;
-    }
-
-    private static int getRandomIntegerWithinRange(int min, int max) {
-        int spread = max - min;
-        return new Random().nextInt(spread + 1) + min;
-    }
-
+ 
 }
