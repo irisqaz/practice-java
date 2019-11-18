284056d9f297b44f9df83165c853dce07529e26c
irisqaz <jgarc172@mail.ccsf.edu>
2019-11-17 19:05:59 -0800

    ready for challenge

--- Fibonacci.java
+++ Fibonacci.java
@@ -20,6 +20,51 @@ public class Fibonacci {
 
         fib[0] = 0;
         fib[1] = 1;
+        fib[2] = fib[1] + fib[0];
+        
+        return fib[2];
+    }
+    public static void main(String[] args) {
+
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+
+            int n = getInt(0, 90);
+            System.out.printf("%4d",n);
+
+            long result = fib(n);
+            long soln = solution(n);
+
+            System.out.printf(" -> %20s ", result);
+            if (result != soln){
+                System.out.printf(" X expected: %s ", soln);
+            } else {
+                System.out.print(" correct");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static int getInt(int min, int max) {
+        int spread = max - min;
+        return new Random().nextInt(spread + 1) + min;
+    }
+    private static long solution(int n) {
+        // when n < 2
+        // fib(0) is 0
+        // fib(1) is 1
+        // when n >= 2
+        // fib(2) is fib(1) + fib(0)
+        //   ^         ^        ^
+        //   |         |        |
+        // fib(3) is fib(2) + fib(1)
+        // fib(4) is fib(3) + fib(2)
+        // . . .
+        long[] fib = new long[3];
+
+        fib[0] = 0;
+        fib[1] = 1;
         if (n < 2){
             return fib[n];
         }
@@ -32,23 +77,6 @@ public class Fibonacci {
         }
         //System.out.print("]");
         return fib[2];
-    }
-    public static void main(String[] args) {
-
-        System.out.println();
-        for (int i = 0; i < 5; i++) {
-
-            int n = getRandomIntegerWithinRange(0, 100);
-            System.out.print(n);
-            long result = fib(n);
-            System.out.printf("  -> %d \n", result);
-        }
-        System.out.println();
-    }
-
-    private static int getRandomIntegerWithinRange(int min, int max) {
-        int spread = max - min;
-        return new Random().nextInt(spread + 1) + min;
     }
 
 }
